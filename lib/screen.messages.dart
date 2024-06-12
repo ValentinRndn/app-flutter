660045419dff1b07847992ea7a6c406540ee31d5
Valentin <valentin.renaudin@my-digital-school.org>
2024-06-12 20:03:25 +0200

    [FIX] Primary color

--- lib/screen.messages.dart
+++ lib/screen.messages.dart
@@ -5,13 +5,8 @@ import 'package:chatbot_filrouge/class/token.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chatbot_filrouge/screen.personnageConversation.dart';
 
-<<<<<<< HEAD
 class ScreenMessages extends StatefulWidget {
   const ScreenMessages({super.key});
-=======
-class MessagesScreen extends StatefulWidget {
-  const MessagesScreen({Key? key}) : super(key: key);
->>>>>>> ab933b48f9d70df647ae2a25fb11f1d0a25deb9f
 
   @override
   State<ScreenMessages> createState() => _ScreenMessagesState();
@@ -45,12 +40,8 @@ class _ScreenMessagesState extends State<ScreenMessages> {
         title: const Text(
           'Messages',
           style: TextStyle(
-<<<<<<< HEAD
             fontSize: 30,
             color: Color.fromARGB(255, 0, 0, 0),
-=======
-            fontSize: 24,
->>>>>>> ab933b48f9d70df647ae2a25fb11f1d0a25deb9f
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -75,15 +66,10 @@ class _ScreenMessagesState extends State<ScreenMessages> {
                   ConnectionState.waiting) {
                 return const Center(child: CircularProgressIndicator());
               } else if (conversationSnapshot.hasError) {
-<<<<<<< HEAD
                 return Center(
                     child: Text('Error: ${conversationSnapshot.error}'));
               } else if (!conversationSnapshot.hasData ||
                   conversationSnapshot.data!.isEmpty) {
-=======
-                return Center(child: Text('Error: ${conversationSnapshot.error}'));
-              } else if (!conversationSnapshot.hasData || conversationSnapshot.data!.isEmpty) {
->>>>>>> ab933b48f9d70df647ae2a25fb11f1d0a25deb9f
                 return const Center(child: Text('No conversations found'));
               }
 
@@ -93,18 +79,12 @@ class _ScreenMessagesState extends State<ScreenMessages> {
                 itemCount: conversations.length,
                 itemBuilder: (context, index) {
                   final conversation = conversations[index];
-<<<<<<< HEAD
                   final characterName =
                       conversation['character_name'] ?? 'Nom personnage';
                   final universeName =
                       conversation['universe_name'] ?? 'Nom univers';
                   final characterImage = conversation['character_image'] ??
                       'https://via.placeholder.com/75';
-=======
-                  final characterName = conversation['character_name'] ?? 'Character Name';
-                  final universeName = conversation['universe_name'] ?? 'Universe Name';
-                  final characterImage = conversation['character_image'] ?? 'https://via.placeholder.com/75';
->>>>>>> ab933b48f9d70df647ae2a25fb11f1d0a25deb9f
                   final characterId = conversation['character_id'] ?? 0;
                   final universId = conversation['universe_id'] ?? 0;
                   final userId = conversation['user_id'] ?? 0;
@@ -125,7 +105,6 @@ class _ScreenMessagesState extends State<ScreenMessages> {
                               SizedBox(
                                 width: 75,
                                 height: 75,
-<<<<<<< HEAD
                                 child: ClipRRect(
                                   borderRadius: BorderRadius.circular(9),
                                   child: CachedNetworkImage(
@@ -138,25 +117,9 @@ class _ScreenMessagesState extends State<ScreenMessages> {
                                       width: 75,
                                       height: 75,
                                       fit: BoxFit.cover,
-=======
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            const SizedBox(width: 15),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    characterName,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
->>>>>>> ab933b48f9d70df647ae2a25fb11f1d0a25deb9f
                                     ),
                                     fit: BoxFit.cover,
                                   ),
-<<<<<<< HEAD
                                 ),
                               ),
                               const SizedBox(width: 10),
@@ -181,29 +144,14 @@ class _ScreenMessagesState extends State<ScreenMessages> {
                                     ),
                                   ],
                                 ),
-=======
-                                  const SizedBox(height: 5),
-                                  Text(
-                                    universeName,
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ],
->>>>>>> ab933b48f9d70df647ae2a25fb11f1d0a25deb9f
                               ),
                             ],
                           ),
                         ),
-<<<<<<< HEAD
                         const Divider(
                           color: Colors.grey,
                         ),
                       ],
-=======
-                      ),
->>>>>>> ab933b48f9d70df647ae2a25fb11f1d0a25deb9f
                     ),
                   );
                 },
